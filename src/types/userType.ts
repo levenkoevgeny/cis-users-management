4cce70f95b1140aedb52294a48e09f8bd9829d70
Levenko Evgeny <levenko.evgeny@yandex.by>
2025-02-09 23:14:24 +0300

    init with redux auth -2

--- src/types/userType.ts
+++ src/types/userType.ts
@@ -1,16 +1,16 @@
 export interface UserTypeBase {
   username: string
 }
-
+// what we get from api -> /api/users/1/ or /api/users/
 export interface UserTypeItem extends UserTypeBase {
   id: number
-  is_staff?: boolean
-  is_superuser?: boolean
-  first_name?: string
-  last_name?: string
-  is_active?: boolean
-  date_joined?: string
-  last_login?: string
+  is_staff: boolean
+  is_superuser: boolean
+  first_name: string
+  last_name: string
+  is_active: boolean
+  date_joined: string | null
+  last_login?: string | null
 }
 
 export interface UserTypeCreate extends UserTypeBase {
